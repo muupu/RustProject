98e23afffae5e33eb1688cf5bfb1b4a625ad5780
muupu <791132272@qq.com>
2022-03-26 21:43:52 +0800

    implement my From<T>and my Into<T>

--- common_trait_14/src/main.rs
+++ common_trait_14/src/main.rs
@@ -14,6 +14,28 @@ enum Language {
     Haskell
 }
 
+pub trait MyFrom<T> {
+    // anonymous parameters are removed in the 2018 edition (see RFC 1685)
+    // fn myfrom(T) -> Self;  // ^ expected one of `:`, `@`, or `|`
+    fn myfrom(_: T) -> Self; 
+}
+
+pub trait MyInto<T> {
+    fn myinto(self) -> T;
+}
+
+impl<T, U> MyInto<U> for T where U: MyFrom<T>  {
+    
+    fn myinto(self) -> U {
+        U::myfrom(self)
+    }
+}
+
+impl MyFrom<&str> for String {
+    fn myfrom(s: &str) -> Self {
+        String::from(s)
+    }
+}
 
 
 fn main() {
@@ -27,4 +49,17 @@ fn main() {
     // name地址不一样 =》说明name字段的堆内存被clone了一份，因此Clone是深度拷贝，栈内存和堆内存都一起拷贝
     println!("dev: {:?}, addr of dev name: {:p}", dev, dev.name.as_str());
     println!("dev1: {:?}, addr of dev1 name: {:p}", dev1, dev1.name.as_str()); 
+
+
+    let s = String::from("hello world!");
+    println!("{}", s);
+
+    let s1 = String::myfrom("hello world111!");
+    println!("{}", s1);
+ 
+    // 编译报错：error[E0282]: type annotations needed
+    // let s2 = "hello world222!".myinto(); // consider giving `s2` a type
+    let s2: String = "hello world222!".myinto();
+    println!("{}", s2);
+    
 }
